9fcfd75b0bc0cbe38015202c03b569fad0f2f0f8
Tak Tran <contact@tutaktran.com>
2020-08-29 17:26:56 +0100

    Move estimate pose function outside of component

--- src/posenet/posenet-hello.js
+++ src/posenet/posenet-hello.js
@@ -9,16 +9,17 @@ const imageStyles = {
   width: "500px",
 };
 
+async function estimatePoseOnImage(imageElement) {
+  const net = await posenet.load();
+
+  const pose = await net.estimateSinglePose(imageElement, {
+    flipHorizontal: false,
+  });
+  return pose;
+}
+
 export function PosenetHello() {
   const imageEl = React.useRef(null);
-  async function estimatePoseOnImage(imageElement) {
-    const net = await posenet.load();
-
-    const pose = await net.estimateSinglePose(imageElement, {
-      flipHorizontal: false,
-    });
-    return pose;
-  }
 
   React.useEffect(() => {
     estimatePoseOnImage(imageEl.current).then((pose) => {
